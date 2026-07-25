4830de828570feb3e90dbfb90377e332083f5524
Stephan Orgiazzi <stephan.orgiazzi@gmail.com>
2026-07-25 19:35:32 +0200

    build: bundle the SDK3 runtime

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = "Stop"
 $projectRoot = Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)
 Set-Location $projectRoot
 
-if (-not (Test-Path "src-tauri\Everything64.dll")) {
+if (-not (Test-Path "src-tauri\Everything3_x64.dll")) {
   & "$PSScriptRoot\install-everything-sdk.ps1"
 }
 if (-not (Test-Path "Cargo.lock")) {
